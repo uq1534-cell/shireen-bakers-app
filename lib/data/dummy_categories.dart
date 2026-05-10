8c7a01235732427ab4c5d119235c2b493d61efcc
uq1534-cell <uq1534@gmail.com>
2026-05-10 22:10:11 +0500

    Updated images

--- lib/data/dummy_categories.dart
+++ lib/data/dummy_categories.dart
@@ -1,39 +1,19 @@
 import '../models/product.dart';
 
 const List<Category> dummyCategories = [
-  Category(id: 'cakes', name: 'Cakes', imageUrl: 'assets/images/cakehtml.jpg'),
-  Category(
-      id: 'bread', name: 'Bread', imageUrl: 'assets/images/bread_category.png'),
-  Category(id: 'snacks', name: 'Snacks', imageUrl: 'assets/images/snacks.jpg'),
-  Category(id: 'donuts', name: 'Donuts', imageUrl: 'assets/images/Donuts.jpg'),
-  Category(
-      id: 'pastries',
-      name: 'Pastries',
-      imageUrl: 'assets/images/moussepastry.jpg'),
-  Category(
-      id: 'biscuits',
-      name: 'Biscuits',
-      imageUrl: 'assets/images/biscuitshtml.png'),
-  Category(
-      id: 'icecream',
-      name: 'Ice Cream',
-      imageUrl: 'assets/images/icecreamcake.jpg'),
-  Category(id: 'rusk', name: 'Rusk', imageUrl: 'assets/images/ruskhtml.jpg'),
-  Category(
-      id: 'cookies',
-      name: 'Cookies & Khatai',
-      imageUrl: 'assets/images/khataihtml.png'),
-  Category(id: 'puffs', name: 'Puffs', imageUrl: 'assets/images/puffhtml.png'),
-  Category(id: 'nimko', name: 'Nimko', imageUrl: 'assets/images/nimkohtml.png'),
-  Category(
-      id: 'chocolate',
-      name: 'Chocolate',
-      imageUrl: 'assets/images/chocolate.png'),
-  Category(
-      id: 'sandwiches',
-      name: 'Sandwiches',
-      imageUrl: 'assets/images/sandwich.jpg'),
-  Category(id: 'salad', name: 'Salad', imageUrl: 'assets/images/salad.png'),
-  Category(
-      id: 'gifting', name: 'Gifting', imageUrl: 'assets/images/gifting.png'),
+  Category(id: 'cakes',      name: 'Cakes',           imageUrl: 'assets/images/cakehtml.png'),
+  Category(id: 'bread',      name: 'Bread',           imageUrl: 'assets/images/bread_category.png'),
+  Category(id: 'snacks',     name: 'Snacks',          imageUrl: 'assets/images/snacks.jpg'),
+  Category(id: 'donuts',     name: 'Donuts',          imageUrl: 'assets/images/glazedoriginal.png'),
+  Category(id: 'pastries',   name: 'Pastries',        imageUrl: 'assets/images/moussepastry.png'),
+  Category(id: 'biscuits',   name: 'Biscuits',        imageUrl: 'assets/images/biscuitshtml.png'),
+  Category(id: 'icecream',   name: 'Ice Cream',       imageUrl: 'assets/images/classicvanilla.png'),
+  Category(id: 'rusk',       name: 'Rusk',            imageUrl: 'assets/images/specialrusk.png'),
+  Category(id: 'cookies',    name: 'Cookies & Khatai',imageUrl: 'assets/images/khataihtml.png'),
+  Category(id: 'puffs',      name: 'Puffs',           imageUrl: 'assets/images/puffhtml.png'),
+  Category(id: 'nimko',      name: 'Nimko',           imageUrl: 'assets/images/mixnimko.png'),
+  Category(id: 'chocolate',  name: 'Chocolate',       imageUrl: 'assets/images/chocolate.png'),
+  Category(id: 'sandwiches', name: 'Sandwiches',      imageUrl: 'assets/images/sandwich.jpg'),
+  Category(id: 'salad',      name: 'Salad',           imageUrl: 'assets/images/salad.png'),
+  Category(id: 'gifting',    name: 'Gifting',         imageUrl: 'assets/images/gifting.png'),
 ];
